f59ea2c81d608d8524c42652cd17de2ae8c475ed
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-02-01 14:34:54 +0530

    side bar skeleton

--- src/pages/Portal.jsx
+++ src/pages/Portal.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Hamburger from '../components/Hamburger'
 
 export default function Portal() {
   return (
+    <>
     <div>Portal</div>
+    <Hamburger/>
+    </>
   )
 }
