bc4b7fdc99510ac50c23cb9b939b44f5b10de684
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-02-01 12:17:24 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import TypingText from './Type'
 export default function Footer() {
   return (
     
@@ -10,7 +10,7 @@ export default function Footer() {
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
                   {/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" /> */}
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Mitr.</span>
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><TypingText/></span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
