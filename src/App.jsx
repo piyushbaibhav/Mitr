73323709b923fc3e2245e859970fff5720030515
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-02-01 10:27:43 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 import DarkModeToggle from './components/DarkMode'
-import TypingText from './components/type'
+import TypingText from './components/Type'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
       <DarkModeToggle/>
       <TypingText/>
+      <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+
+      </Router>
       
     </>
   )
