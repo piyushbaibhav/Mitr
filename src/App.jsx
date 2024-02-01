9911561a23741b95e0ef27a7a5dbfc3705da8bec
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-02-01 09:51:12 +0530

    logo cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import DarkModeToggle from './components/DarkMode'
 import TypingText from './components/type'
