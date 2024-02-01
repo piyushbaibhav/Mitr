37a573879778fd9e45fc0862c3462665d63523ad
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-02-01 12:40:54 +0530

    progress pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import TypingText from './components/Type'
 import Hamburger from './components/Hamburger';
 import Footer from './components/Footer';
 import DocumentVerify from './pages/DocumentVerify';
+import PhotoUpload from './pages/PhotoUpload';
+import Terms from './pages/Terms';
+import Portal from './pages/Portal';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +27,9 @@ function App() {
         <Route path='/' element={<Login/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/documentverify' element={<DocumentVerify/>}/>
+        <Route path='/photoupload' element={<PhotoUpload/>}/>
+        <Route path='/term' element={<Terms/>}/>
+        <Route path='/portal' element={<Portal/>}/>
       </Routes>
         <Footer/>
       </Router>
