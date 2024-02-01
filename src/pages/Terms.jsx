f94be1246b2b46d9107dac262ad001876eaa422d
Vedant Patil <vedant1346@gmail.com>
2024-02-01 17:38:08 +0530

    Terms Page Update

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Terms() {
   return (
     <>
-        <nav className='m-5'>
+        <nav className='mt-10 ml-5 mb-5'>
         <ol class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
           <li class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
             <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
@@ -39,11 +39,38 @@ export default function Terms() {
           </li>
         </ol>
     </nav>
-    <div className='ml-5 mr-20 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum asperiores perspiciatis nemo error ipsum tenetur unde eum voluptatem quam iusto eligendi odit sequi, ratione commodi! Rerum, modi aperiam! Earum, quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga qui minus laboriosam labore facilis laudantium ducimus esse odio excepturi cupiditate, repellat quae quas porro sapiente necessitatibus doloribus praesentium, unde numquam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni voluptate dolor ullam libero veritatis? Repellendus voluptatem temporibus accusamus quasi dicta unde magni ducimus minima pariatur dolor officiis, expedita non rem!
+    <div className='ml-5 mr-20 text-justify'>
+      <p><strong>Terms and Conditions</strong></p>
+      <p>Our Terms and Conditions were last updated on Feb 2024.</p>
+      <p>Please read these terms and conditions carefully before using Our Service.</p>
+      <p><strong>Interpretation and Definitions</strong></p>
+      <p><strong>Interpretation</strong></p>
+      <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
+      <p><strong>Definitions</strong></p>
+      <p>For the purposes of these Terms and Conditions:</p>
+          <div className="flex mt-2 items-center">
+            <input
+              id="nda-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" 
+            />
+            <label
+              htmlFor="nda-checkbox"
+              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              I agree with the{' '}
+              <a href="https://s3.us-east-2.amazonaws.com/myhrcounsel2/On-boarding/Forms/Employee+Confidentiality+and+Non-Disclosure+Agreement.pdf" target="_blank" className="text-blue-600 dark:text-blue-500 hover:underline">
+                T&C
+              </a>
+              .
+            </label>
+          </div>
+
     </div>
 
     <Link to="/portal">
-         <button type="button" class="text-white ml-5 mt-5 mb-60 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+         <button type="button" class="text-white ml-5 mt-5 mb-56 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Proceed</button>
     </Link>
     </>
   )
