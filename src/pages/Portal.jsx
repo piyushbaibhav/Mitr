eadad3ffda595c98ee4446ff229769db0ab292d2
Vedant Patil <vedant1346@gmail.com>
2024-02-01 15:44:14 +0530

    Portal update

--- src/pages/Portal.jsx
+++ src/pages/Portal.jsx
@@ -4,8 +4,19 @@ import Hamburger from '../components/Hamburger'
 export default function Portal() {
   return (
     <>
-    <div>Portal</div>
-    <Hamburger/>
+    <div className='flex'>    
+      <div className='ml-96 mt-10 pb-4 mb-72'>Technical Integrations soon to be included in this App <br/>
+      Firebase for backend <br/>
+       For Real-time Database <br/>
+       Authentication <br/>
+       Cloud Functions <br/>
+       File Storage <br/>
+
+      </div>
+      <div className="relative -z-10"><Hamburger/></div>
+      
+    </div>
+
     </>
   )
 }
