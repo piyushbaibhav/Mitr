83903cddeaa6fc3ed95bfbc3b02e41d364c635da
Vedant Patil <vedant1346@gmail.com>
2024-02-01 15:35:56 +0530

    Dashboard Update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,35 +18,37 @@ export default function Dashboard() {
 
   return (
     <>
-    <nav className='m-5'>
-        <ol class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
-          <li class="flex md:w-full items-center text-green-500 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-            <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-              <span class="me-2">1</span>
-              Contract  <span class="hidden sm:inline-flex sm:ms-2">Acceptance</span>
-            </span>
-          </li>
-          <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-            <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-              <span class="me-2">2</span>
-              Document <span class="hidden sm:inline-flex sm:ms-2">Verification</span>
-            </span>
-          </li>
-          <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-            <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-              <span class="me-2">3</span>
-              Photo <span class="hidden sm:inline-flex sm:ms-2">Upload</span>
-            </span>
-          </li>
-          <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-            <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-              <span class="me-2">4</span>
-              Terms <span class="hidden sm:inline-flex sm:ms-2"> & </span> <span class="hidden sm:inline-flex sm:ms-2"> Conditions</span>
-            </span>
-          </li>
+    <div className=''>
+      
+      <nav className='m-5'>
+          <ol class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+            <li class="flex md:w-full items-center text-green-500 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+              <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+                <span class="me-2">1</span>
+                Contract  <span class="hidden sm:inline-flex sm:ms-2">Acceptance</span>
+              </span>
+            </li>
+            <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+              <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+                <span class="me-2">2</span>
+                Document <span class="hidden sm:inline-flex sm:ms-2">Verification</span>
+              </span>
+            </li>
+            <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+              <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+                <span class="me-2">3</span>
+                Photo <span class="hidden sm:inline-flex sm:ms-2">Upload</span>
+              </span>
+            </li>
+            <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+              <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+                <span class="me-2">4</span>
+                Terms <span class="hidden sm:inline-flex sm:ms-2"> & </span> <span class="hidden sm:inline-flex sm:ms-2"> Conditions</span>
+              </span>
+            </li>
 
-        </ol>
-    </nav>
+          </ol>
+      </nav>
       <div className='m-5'>
         <p className='text-2xl font-bold'>Contract</p>
         <div className='w-4/5 '>
@@ -108,6 +110,7 @@ export default function Dashboard() {
           </button>
         </Link>
       </div>
+    </div>
     </>
   )
 }
