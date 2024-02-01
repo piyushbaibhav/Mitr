c85824f8939f00a538463067325cb7d9631468ee
Vedant Patil <vedant1346@gmail.com>
2024-02-02 05:17:26 +0530

    Touch Up

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -17,7 +17,7 @@ export default function Hamburger() {
         </div>
         <ul class="space-y-5 text-md">
             <li> <Link to='/portal'>
-                <a href="#" class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 bg-gray-200 focus:shadow-outline">
+                <a href="#" class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 focus:bg-gray-200 focus:shadow-outline">
                     <span class="text-gray-600">
                         <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
