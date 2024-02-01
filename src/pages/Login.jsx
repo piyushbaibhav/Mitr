0c7609fa266d11d90042b5feb71bf61532cf28a2
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-02-02 00:51:18 +0530

    firebase login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,57 @@
-import React, { useState } from 'react';
 import './Login.css';
+
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../firebase/config';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { 
+          createUserWithEmailAndPassword, 
+          signInWithEmailAndPassword
+       } from "firebase/auth";
 
 
 const Login = () => {
   const [isActive, setIsActive] = useState(false);
   const [userCredentials, setUserCredentials]=useState({});
+  const [error, setError]=useState('');
 
   function handleCredentials(e){
     setUserCredentials({...userCredentials,[e.target.name]: e.target.value});
-    console.log(userCredentials);
+    
   }
 
   function handleSignup(e){
     e.preventDefault();
+    setError("");
+
     createUserWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
   .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    console.log(user);
+    
+    console.log(userCredential.user);
+    
     // ...
   })
   .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
+    
+    setError(error.message);
+    
     // ..
   });
 
+  }
+  function handleLogin(e){
+    e.preventDefault();
+    setError("");
+    signInWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
+    .then((userCredential) => {
+      // Signed in 
+      console.log(userCredential.user);
+      // ...
+    })
+    .catch((error) => {
+      console.error('Sign-in error:', error);
+      setError(error.message);
+    });
   }
 
   const handleRegisterClick = () => {
@@ -48,40 +69,7 @@ const Login = () => {
         <form>
           <h1>Create Account</h1>
 
-          <div className="social-icons">
-            <a href="#" class="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 488 512"
-                className='w-1/2 h-1/2 '
-              >
-                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-              </svg>
-            </a>
-            <a href="#" class="icon">
-              <svg xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-                className='w-1/2 h-1/2 '>
-                <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
-              </svg>
-            </a>
-            <a href="#" className="icon">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                className='w-1/2 h-1/2'>
-                <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
-                </svg>
-            </a>
-            <a href="#" class="icon">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 496 512"
-                className='w-1/2 h-1/2'>
-                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-                </svg>
-            </a>
-          </div>
+          
           <span>or use your email for registration</span>
           {/* <input onChange={(e)=>{handleCredentials(e)}} type="text" placeholder="Name" /> */}
           <input onChange={(e)=>{handleCredentials(e)}} type="text" name="email" placeholder="Email" />
@@ -89,6 +77,14 @@ const Login = () => {
           <Link to="/dashboard">
           <button onClick={(e)=>{handleSignup(e)}}>Sign Up</button>
           </Link>
+          {
+            error &&
+            <div className='text-red-500'>
+              {error}
+            </div>
+          }
+
+          
         </form>
       </div>
       <div className={`form-cont sign-in ${isActive ? '' : 'move'}`}>
@@ -96,47 +92,21 @@ const Login = () => {
         <form>
           <h1>Sign In</h1>
 
-          <div className="social-icons">
-            <a href="#" class="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 488 512"
-                className='w-1/2 h-1/2 '
-              >
-                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-              </svg>
-            </a>
-            <a href="#" className="icon">
-              <svg xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-                className='w-1/2 h-1/2 '>
-                <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
-              </svg>
-            </a>
-            <a href="#" class="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                className='w-1/2 h-1/2'>
-                <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
-                </svg>
-            </a>
-            <a href="#" class="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 496 512"
-                className='w-1/2 h-1/2'>
-                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-                </svg>
-            </a>
-          </div>
+          
           <span>or use your email and password</span>
-          <input  type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input  type="text" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
           <a href="">Forgot your email or password?</a>
           <Link to="/dashboard">
-          <button>Sign in</button>
+          <button onClick={(e)=>{handleLogin(e)}} >Sign in</button>
           </Link>
+
+          {
+            error &&
+            <div className='text-red-500'>
+              {error}
+            </div>
+          }
 
         </form>
       </div>
@@ -159,21 +129,7 @@ const Login = () => {
         </div>
       </div>
 
-      {/* <button
-        id="register"
-        className={`your-register-button-styles ${isActive ? 'hidden' : ''}`}
-        onClick={handleRegisterClick}
-      >
-        Register
-      </button>
-
-      <button
-        id="login"
-        className={`your-login-button-styles ${isActive ? '' : 'hidden'}`}
-        onClick={handleLoginClick}
-      >
-        Login
-      </button> */}
+      
     </div>
   );
 };
