13d3a064771076495590c9911d4107f58e7e7984
Vedant Patil <vedant1346@gmail.com>
2024-02-02 03:26:15 +0530

    More Pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function About() {
+    return (
+        <div className="">About</div>
+    )
+}
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Settings() {
+    return (
+        <div className="">Settings</div>
+    )
+}
