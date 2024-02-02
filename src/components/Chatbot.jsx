b518bc0eb71017cda67f22f383d0ab8fef84d998
Augnik <shresnik2004@gmail.com>
2024-02-02 06:17:12 +0530

    chatbot update

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -35,7 +35,7 @@ const Chatbot = () => {
     };
 
     return (
-        <div className={`absolute bottom-4 right-4 z-50 transition-transform ${chatOpen ? 'transform translate-y-0' : 'transform translate-y-0'}`}>
+        <div className={`fixed bottom-4 right-4 z-50 transition-transform ${chatOpen ? 'transform translate-y-0' : 'transform translate-y-0'}`}>
             <div className="bg-blue-400 text-white p-3 rounded-full cursor-pointer" onClick={toggleChat}>
                 {chatOpen ? (
                     <svg
@@ -48,7 +48,7 @@ const Chatbot = () => {
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 512 512"
-                        className='w-10 h-6'>
+                        className='w-10 h-6 '>
                         <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
                     </svg>
                 )}
