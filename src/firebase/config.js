59a147854c410ab0358c78fe7299b3301b363057
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-02-03 18:56:01 +0530

    firebase import fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getStorage } from 'firebase/storage'
-import { getFirestore } from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 // import { getFirestore } from "firebase/firestore";
-
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -24,6 +23,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 // const db = getFirestore(app);
 const imgDB = getStorage(app);
-const txtDB =getFirestore(app);
+const txtDB = getFirestore(app);
 
-export {app, auth, imgDB, txtDB};
+export { app, auth, imgDB, txtDB };
